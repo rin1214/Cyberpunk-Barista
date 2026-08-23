e36df3a7532df546cb6ea885cd7b63cbca71c9b7
Nurin Farzana <nfarzana1412@gmail.com>
2026-08-23 18:18:45 +0800

    Initial commit: Set up main Pygame window and game clock

---
+++ main.py
@@ -0,0 +1,39 @@
+import pygame
+import sys
+
+# Start Pygame
+pygame.init()
+
+# Configure the window size
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Cyberpunk Barista - Game Engine")
+
+#  Clock & FPS Engine
+clock = pygame.time.Clock()
+FPS = 60
+
+# Main Game Loop
+running = True
+while running:
+
+    # Delta Time Calculation 
+    # clock.tick(60) caps game at 60 FPS and returns elapsed milliseconds.
+    # Dividing by 1000.0 converts milliseconds into seconds (e.g., 0.016s).
+    dt = clock.tick(FPS) / 1000.0
+
+    # Event handling loop (checks for user inputs)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:  # User clicks the X button
+            running = False
+
+    # Paint the screen background dark gray (RGB: 20, 20, 30)
+    screen.fill((20, 20, 30))
+
+    # Update the display to show what was drawn
+    pygame.display.flip()
+
+# Clean exit
+pygame.quit()
+sys.exit()
